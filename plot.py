6530caf5743fd3ce2736487f0400c6cdaa37555d
Teddy Ward <emw35@duke.edu>
2015-09-13 17:00:58 -0400

    better tools

--- plot.py
+++ plot.py
@@ -13,13 +13,14 @@ CO_table = emissions.getCOData()
 # output to static HTML file
 output_file("plots/CO.html", title="Carbon Monoxide Emissions")
 
+TOOLS="resize,crosshair,tap,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select,hover";
+
 # create a new plot with a title and axis labels
-p = figure(title="Carbon Monoxide", x_axis_label='Year', y_axis_label='CO Emissions')
+p = figure(title="Carbon Monoxide", x_axis_label='Year', y_axis_label='CO Emissions', tools=TOOLS);
+
+colors = ["#660033", "#FF0066", "#FFCC99", "#CCCC00", "#333300", "#00FF00", "#009999", "#66FFFF", "#000099", "#6600CC", "#CC7A00", "#FF9999", "#FFFF00", "#522900", "#006600"];
 
 # add a line renderer with legend and line thickness
 for region in range(15):
-    p.circle(years, CO_table[region, :], color="#00" + str(region*(75/15)) + "00", size=10, alpha=0.5);
-    p.line(years, CO_table[region, :], legend="region " + str(region), line_color="#00" + str(region*(75/15)) + "00", line_width=2);
-
-# show the results
-save(p)
+    p.circle(years, CO_table[region, :], color=colors[region], size=10, alpha=0.5);
+    p.line(years, CO_table[region, :], legend="region " + str(region), line_color=colors[region], line_width=2);
