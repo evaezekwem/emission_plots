d942f7ee9410dbc7149d28e9f6af1687d135decd
Teddy Ward <emw35@duke.edu>
2015-09-29 17:43:07 -0400

    corrected all charts

--- get_GFEDS_all_emissions.py
+++ get_GFEDS_all_emissions.py
@@ -1,12 +1,15 @@
+from plot import Plotter
 import csv
 import numpy as np
 import h5py # if this creates an error please make sure you have the h5py library
 
-months       = '01','02','03','04','05','06','07','08','09','10','11','12'
-regions      = 'BONA', 'TENA', 'CEAM', 'NHSA', 'SHSA', 'EURO', 'MIDE', 'NHAF', 'SHAF', 'BOAS', 'TEAS', 'SEAS', 'EQAS', 'AUST', 'Global';
-sources      = 'SAVA','BORF','TEMF','DEFO','PEAT','AGRI', 'All sources';
-species_used = 'CO2', 'CH4', 'BC', 'SO2', 'CO', 'OC', 'N2O', 'NOx', 'NH3'
-species_row  = 2, 4, 13, 14, 3, 12, 8, 7, 33
+def calculate_emissions():
+    months       = '01','02','03','04','05','06','07','08','09','10','11','12'
+    regions      = ['BONA', 'TENA', 'CEAM', 'NHSA', 'SHSA', 'EURO', 'MIDE', 'NHAF', 'SHAF', 'BOAS', 'TEAS', 'SEAS', 'EQAS', 'AUST', 'Global'];
+    sources      = 'SAVA','BORF','TEMF','DEFO','PEAT','AGRI', 'All sources';
+    species_used = 'CO2', 'CH4', 'BC', 'SO2', 'CO', 'OC', 'N2O', 'NOx', 'NH3'
+    species_row  = 2, 4, 13, 14, 3, 12, 8, 7, 33
+    scar_values = [84, 4589, 270419, 41968, 632, 68299, 36987, 67141, 24906];
 
 # in this example we will calculate annual CO emissions for the 14 GFED 
 # basisregions over 1997-2014. Please adjust the code to calculate emissions
@@ -14,92 +17,107 @@ species_row  = 2, 4, 13, 14, 3, 12, 8, 7, 33
 # first download the GFED4.1s files and the GFED4_Emission_Factors.txt
 # to your computer and adjust the directory where you placed them below
 
-directory    = '.'
+    directory    = '.'
 
 
-"""
-Read in emission factors
-"""
-species = [] # names of the different gas and aerosol species
-EFs     = np.zeros((41, 6)) # 41 species, 6 sources
+    """
+    Read in emission factors
+    """
+    species = [] # names of the different gas and aerosol species
+    EFs     = np.zeros((41, 6)) # 41 species, 6 sources
 
-k = 0
-f = open(directory+'/GFED4_Emission_Factors.txt')
-while 1:
-    line = f.readline()
-    if line == "":
-        break
+    k = 0
+    f = open(directory+'/GFED4_Emission_Factors.txt')
+    while 1:
+        line = f.readline()
+        if line == "":
+            break
+            
+        if line[0] != '#':
+            contents = line.split()
+            species.append(contents[0])
+            EFs[k,:] = contents[1:]
+            k += 1
+                    
+    f.close()
+
+    start_year = 1997
+    end_year   = 2014
+    plotter = Plotter();
+
+    for species_num in range(9):
+        EF_species = EFs[species_row[species_num]];
+        emissions_writer = csv.writer(open("./plots/tables/emissions/" + species_used[species_num] + '_emissions.csv', 'w'));
+        scar_writer = csv.writer(open("./plots/tables/SCAR/" + species_used[species_num] + '_scar.csv','w'));
+        scar_writer.writerow(["units - 2007 US$"]);
+        emissions_writer.writerow(["units - 1E12 g"]);
+        for year in range(start_year, end_year+1):
+            emissions_table = np.zeros((7, 15)) # source, region
+            source_emissions = np.zeros(7);
+            emissions_writer.writerow([year]);
+            emissions_writer.writerow([""] + regions);
+            scar_writer.writerow([year]);
+            scar_writer.writerow([""] + regions);
         
-    if line[0] != '#':
-        contents = line.split()
-        species.append(contents[0])
-        EFs[k,:] = contents[1:]
-        k += 1
-                
-f.close()
-
-start_year = 1997
-end_year   = 2014
-
-for species_num in range(9):
-    EF_species = EFs[species_row[species_num]];
-    writer = csv.writer(open("./plots/data/" + species_used[species_num] + '_data.csv', 'w'));
-    for year in range(start_year, end_year+1):
-        emissions_table = np.zeros((7, 15)) # source, region
-        source_emissions = np.zeros(7);
-        header = [year];
-        writer.writerow(header);
-        writer.writerow(regions);
-	
-        string = directory+'/GFED4.1s_'+str(year)+'.hdf5'
-        f = h5py.File(string, 'r')
-        if year == start_year: # these are time invariable    
-            basis_regions = f['/ancill/basis_regions'][:]
-            grid_area     = f['/ancill/grid_cell_area'][:]
-        
-        total_emissions = np.zeros((720, 1440));
-	for month in range(12):
-	    # read in DM emissions
-	    string = '/emissions/'+months[month]+'/DM'
-	    DM_emissions = f[string][:]
-	    for source in range(6):
+            string = directory+'/GFED4.1s_'+str(year)+'.hdf5'
+            f = h5py.File(string, 'r')
+            if year == start_year: # these are time invariable    
+                basis_regions = f['/ancill/basis_regions'][:]
+                grid_area     = f['/ancill/grid_cell_area'][:]
+            
+            total_emissions = np.zeros((720, 1440));
+            for source in range(6):
                 source_emissions = np.zeros((720, 1440))
-	        # read in the fractional contribution of each source
-	        string = '/emissions/'+months[month]+'/partitioning/DM_'+sources[source]
-	        contribution = f[string][:]
-	        source_emissions = DM_emissions * contribution * EF_species[source];
-		# calculate CO emissions as the product of DM emissions (kg DM per 
-	        # m2 per month), the fraction the specific source contributes to 
-	        # this (unitless), and the emission factor (g CO per kg DM burned)
-	        total_emissions += source_emissions;
+                for month in range(12):
+                    # read in DM emissions
+                    string = '/emissions/'+months[month]+'/DM'
+                    DM_emissions = f[string][:]
+                    # read in the fractional contribution of each source
+                    string = '/emissions/'+months[month]+'/partitioning/DM_'+sources[source]
+                    contribution = f[string][:]
+                    source_emissions += DM_emissions * contribution * EF_species[source];
+                # calculate CO emissions as the product of DM emissions (kg DM per 
+                    # m2 per month), the fraction the specific source contributes to 
+                    # this (unitless), and the emission factor (g CO per kg DM burned)
+                total_emissions += source_emissions;
                 for region in range(15):
-		    if region == 14:
-			mask = np.ones((720, 1440))
-		    else:
-			mask = basis_regions == (region + 1)            
-		    emissions_table[source, region] = np.sum(grid_area * mask * source_emissions);
+                    if region == 14:
+                        mask = np.ones((720, 1440))
+                    else:
+                        mask = basis_regions == (region + 1)            
+                    emissions_table[source, region] = np.sum(grid_area * mask * source_emissions);
 
-        
-        # fill table with total values for the globe (row 15) or basisregion (1-14)
-        for region in range(15):
-            if region == 14:
-                mask = np.ones((720, 1440))
-            else:
-                mask = basis_regions == (region + 1)            
-            emissions_table[6, region] = np.sum(grid_area * mask * total_emissions);
-        print "\t" + str(year) + " done";
-        # convert to Tg CO 
-        emissions_table = emissions_table / 1E12;
-        # write
-        for source in range(7):
-            source_list = emissions_table[source, :].tolist();
-            source_list.insert(0, sources[source]);
-            writer.writerow(source_list);
-            #writer.writerow([sources[source]] + emissions_table[source, :].tolist());
-        writer.writerow([]);
-        print emissions_table
-    print species_used[species_num] + " done";
 
-print CO_table
+            
+            # fill table with total values for the globe (row 15) or basisregion (1-14)
+            for region in range(15):
+                if region == 14:
+                    mask = np.ones((720, 1440))
+                else:
+                    mask = basis_regions == (region + 1)            
+                emissions_table[6, region] = np.sum(grid_area * mask * total_emissions);
+            print "\t" + str(year) + " done";
+            # convert to Tg CO 
+            emissions_table = emissions_table / 1E12;
+            # convert to $ value
+            GRAMS_PER_TON = 907185;
+            scar_table = emissions_table * scar_values[species_num] / GRAMS_PER_TON;
+            plotter.plot(species_used[species_num], year, "emissions", emissions_table);
+            plotter.plot(species_used[species_num], year, "SCAR", scar_table);
+            # write
+            for source in range(7):
+                emissions_source_list = emissions_table[source, :].tolist();
+                scar_source_list = scar_table[source, :].tolist();
+                emissions_source_list.insert(0, sources[source]);
+                scar_source_list.insert(0, sources[source]);
+                emissions_writer.writerow(emissions_source_list);
+                scar_writer.writerow(scar_source_list);
+                #writer.writerow([sources[source]] + emissions_table[source, :].tolist());
+            emissions_writer.writerow([]);
+            scar_writer.writerow([]);
+            print emissions_table
+        print species_used[species_num] + " done";
 
 # please compare this to http://www.falw.vu/~gwerf/GFED/GFED4/tables/GFED4.1s_CO.txt
+if __name__ == '__main__':
+        calculate_emissions()
--- plot.py
+++ plot.py
@@ -1,27 +1,24 @@
-from get_GFED4s_CO_emissions import COEmissions;
-from bokeh.plotting import figure, output_file, show
+from bokeh.charts import Bar, output_file, show
+from bokeh.plotting import figure, output_file, save
 import numpy as np;
+import pandas;
 
+class Plotter:
+    
+    def plot(self, species, year, chart, species_table):
+        TOOLS="resize,crosshair,tap,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select,hover";
+        regions      = ['BONA', 'TENA', 'CEAM', 'NHSA', 'SHSA', 'EURO', 'MIDE', 'NHAF', 'SHAF', 'BOAS', 'TEAS', 'SEAS', 'EQAS', 'AUST'];
+        sources      = 'SAVA','BORF','TEMF','DEFO','PEAT','AGRI', 'All sources';
+        region_colors = ["#660033", "#FF0066", "#FFCC99", "#CCCC00", "#333300", "#00FF00", "#009999", "#66FFFF", "#000099", "#6600CC", "#CC7A00", "#FF9999", "#FFFF00", "#522900", "#006600"];
+        
+        without_global = species_table[0:7, 0:14];
+        formatted_data = pandas.DataFrame(data=without_global, index=sources, columns=regions);
 
-start_year = 1997
-end_year = 2014
-
-years = [1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014];
-
-emissions = COEmissions()
-CO_table = emissions.getCOData()
-
-# output to static HTML file
-output_file("plots/CO.html", title="Carbon Monoxide Emissions")
-
-TOOLS="resize,crosshair,tap,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select,hover";
-
-# create a new plot with a title and axis labels
-p = figure(title="Carbon Monoxide", x_axis_label='Year', y_axis_label='CO Emissions', tools=TOOLS);
-
-colors = ["#660033", "#FF0066", "#FFCC99", "#CCCC00", "#333300", "#00FF00", "#009999", "#66FFFF", "#000099", "#6600CC", "#CC7A00", "#FF9999", "#FFFF00", "#522900", "#006600"];
-
-# add a line renderer with legend and line thickness
-for region in range(15):
-    p.circle(years, CO_table[region, :], color=colors[region], size=10, alpha=0.5);
-    p.line(years, CO_table[region, :], legend="region " + str(region), line_color=colors[region], line_width=2);
+        chart_title = "" + species + " " + chart + " - " + str(year);
+        y_label = "Total emissions (1E12 g)";
+        if(chart == "SCAR"):
+            y_label = "Social Cost (2007 US $)";
+        output_file("plots/tables/" + chart + "/plots/" + species + "_" + str(year) + "_" + chart + "_visualization.html", title = chart_title);
+        
+        p = Bar(formatted_data, title=chart_title, xlabel="Source", ylabel=y_label, legend="top_left", stacked=True, palette=region_colors, tools=TOOLS);
+        save(p);
